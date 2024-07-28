ad5a28799db43883c4f43a8847604056b55a2a24
MegaBytesMe <57240557+megabytesme@users.noreply.github.com>
2024-07-28 22:48:29 +0100

    Added PlayerDto class for relevant player data

--- Client/src/client/java/minelights/minelights/PlayerDataProcessor.java
+++ Client/src/client/java/minelights/minelights/PlayerDataProcessor.java
@@ -5,25 +5,37 @@ import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.world.World;
 
 public class PlayerDataProcessor {
-    
-        public void processPlayerData(MinecraftClient client) {
+    public void processPlayerData(MinecraftClient client) {
+        // Check if the client is null
         if (client.world == null || client.player == null) {
             return;
         }
 
+        // Get the player and world
         ClientPlayerEntity player = client.player;
         World world = client.world;
 
-        System.out.println(player.getHealth());
-        String worldLevel = world.getRegistryKey().getValue().getPath();
-        System.out.println("Current world level: " + worldLevel);
-
-        if (world.getBlockState(player.getBlockPos()).getBlock() == net.minecraft.block.Blocks.NETHER_PORTAL) {
-            System.out.println("Player is standing in a nether portal");
-        }
-
+        // Set the player data
+        PlayerDto playerDto = new PlayerDto();
+        playerDto.setWorldLevel(world.getRegistryKey().getValue().getPath(););
+        playerDto.setHealth(player.getHealth());
+        playerDto.setHunger(player.getHungerManager().getFoodLevel());
         if (world.isRaining()) {
-            System.out.println("Rain is falling in the world");
+            if (world.isThundering()) {
+            playerDto.setWeather("Thunderstorm");
+            } else {
+            playerDto.setWeather("Rain");
+            }
+        } else {
+            playerDto.setWeather("Clear");
         }
+        playerDto.setCurrentBlock(world.getBlockState(player.getBlockPos()).getBlock().getTranslationKey());
+
+        // Print the player data
+        System.out.println(playerDto.getWorldLevel());
+        System.out.println(playerDto.getHealth());
+        System.out.println(playerDto.getHunger());
+        System.out.println(playerDto.getWeather());
+        System.out.println(playerDto.getCurrentBlock());        
     }
 }
---
+++ Client/src/client/java/minelights/minelights/PlayerDto.java
@@ -0,0 +1,49 @@
+package minelights.minelights;
+
+public class PlayerDto {
+    private String worldLevel;
+    private float health;
+    private int hunger;
+    private String weather;
+    private String currentBlock;
+
+    public String getWorldLevel() {
+        return worldLevel;
+    }
+
+    public void setWorldLevel(String worldLevel) {
+        this.worldLevel = worldLevel;
+    }
+
+    public float getHealth() {
+        return health;
+    }
+
+    public void setHealth(float health) {
+        this.health = health;
+    }
+
+    public int getHunger() {
+        return hunger;
+    }
+
+    public void setHunger(int hunger) {
+        this.hunger = hunger;
+    }
+
+    public String getWeather() {
+        return weather;
+    }
+
+    public void setWeather(String weather) {
+        this.weather = weather;
+    }
+
+    public String getCurrentBlock() {
+        return currentBlock;
+    }
+
+    public void setCurrentBlock(String currentBlock) {
+        this.currentBlock = currentBlock;
+    }
+}
