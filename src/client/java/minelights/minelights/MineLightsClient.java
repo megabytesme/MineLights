6758f31c422b1244e0c88bc1057d29428f81f88b
MegaBytesMe <57240557+megabytesme@users.noreply.github.com>
2024-07-30 13:07:04 +0100

    Added player connection class to send last packet after server disconnect

--- src/client/java/minelights/minelights/MineLightsClient.java
+++ src/client/java/minelights/minelights/MineLightsClient.java
@@ -1,7 +1,9 @@
 package minelights.minelights;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 
 public class MineLightsClient implements ClientModInitializer {
     @Override
@@ -15,10 +17,27 @@ public class MineLightsClient implements ClientModInitializer {
         }
 
         PlayerDataProcessor playerDataProcessor = new PlayerDataProcessor();
+        PlayerConnectionHandler playerConnectionHandler = new PlayerConnectionHandler();
 
         // This entrypoint is suitable for setting up client-specific logic, such as rendering.
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             playerDataProcessor.processPlayerData(client);
+        });
+
+        ClientPlayConnectionEvents.DISCONNECT.register((handler, client) -> {
+            playerConnectionHandler.onDisconnect();
+        });
+
+        // stop the MineLights helper process when the client is stopping
+        ClientLifecycleEvents.CLIENT_STOPPING.register(client -> {
+            try {
+                String processName = "MineLights.exe";
+                String command = "taskkill /F /IM " + processName;
+                Process process = Runtime.getRuntime().exec(command);
+                process.waitFor();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         });
     }
 }
---
+++ src/client/java/minelights/minelights/PlayerConnectionHandler.java
@@ -0,0 +1,24 @@
+package minelights.minelights;
+
+import com.google.gson.Gson;
+
+public class PlayerConnectionHandler {
+    public void onDisconnect() {
+        PlayerDto playerDto = new PlayerDto();
+        playerDto.setInGame(false);
+
+        // initialize the player data to default values
+        playerDto.setHealth(0);
+        playerDto.setHunger(0);
+        playerDto.setWeather("Clear");
+        playerDto.setCurrentBlock("minecraft:air");
+        playerDto.setCurrentBiome("minecraft:plains");
+
+        // Serialize playerDto to JSON
+        Gson gson = new Gson();
+        String playerDtoJson = gson.toJson(playerDto);
+
+        // Send the JSON data via UDP
+        UDPClient.sendPlayerData(playerDtoJson);
+    }    
+}
