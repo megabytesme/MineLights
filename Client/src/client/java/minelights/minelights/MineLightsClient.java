dc32bfc27e8c28ce000f05b0ec434e0296f9b4ca
MegaBytesMe <57240557+megabytesme@users.noreply.github.com>
2024-07-30 04:25:15 +0100

    Updated client to auto-start MineLights helper service on game launch

--- Client/src/client/java/minelights/minelights/MineLightsClient.java
+++ Client/src/client/java/minelights/minelights/MineLightsClient.java
@@ -6,6 +6,14 @@ import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 public class MineLightsClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
+        try {
+            String modsFolder = System.getProperty("user.dir") + "/mods";
+            ProcessBuilder processBuilder = new ProcessBuilder(modsFolder + "/MineLights.exe");
+            processBuilder.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         PlayerDataProcessor playerDataProcessor = new PlayerDataProcessor();
 
         // This entrypoint is suitable for setting up client-specific logic, such as rendering.
