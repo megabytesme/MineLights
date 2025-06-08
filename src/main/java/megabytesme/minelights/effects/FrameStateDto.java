0c02ca6355977b53c1f24a51f05a4a1f25006a11
MegaBytesMe <57240557+megabytesme@users.noreply.github.com>
2025-06-08 20:39:02 +0100

    Migrate DTOs from server to client

---
+++ src/main/java/megabytesme/minelights/effects/FrameStateDto.java
@@ -0,0 +1,8 @@
+package megabytesme.minelights.effects;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FrameStateDto {
+    public Map<Integer, RGBColorDto> keys = new HashMap<>();
+}
---
+++ src/main/java/megabytesme/minelights/effects/KeyMap.java
@@ -0,0 +1,30 @@
+package megabytesme.minelights.effects;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class KeyMap {
+    public static List<String> getExperienceBar() {
+        return Arrays.asList("1", "2", "3", "4", "5", "6", "7", "8", "9", "0");
+    }
+
+    public static List<String> getHealthBar() {
+        return Arrays.asList("F1", "F2", "F3", "F4");
+    }
+
+    public static List<String> getHungerBar() {
+        return Arrays.asList("F5", "F6", "F7", "F8");
+    }
+
+    public static List<String> getMovementKeys() {
+        return Arrays.asList("W", "A", "S", "D", "LCTRL", "LSHIFT", "SPACE");
+    }
+
+    public static List<String> getHeartbeatRed() {
+        return Arrays.asList("4", "5", "6", "7", "E", "T", "U", "D", "F", "H", "V", "B", "SPACE");
+    }
+
+    public static List<String> getHeartbeatWhite() {
+        return Arrays.asList("R", "Y", "G");
+    }
+}
