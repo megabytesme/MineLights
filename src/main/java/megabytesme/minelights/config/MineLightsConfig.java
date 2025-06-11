5f40962d3c8cdf9f4f72fddb691e79291c868ab5
MegaBytesMe <57240557+megabytesme@users.noreply.github.com>
2025-06-11 05:42:04 +0100

    Added additional SDKs to client via server

--- src/main/java/megabytesme/minelights/config/MineLightsConfig.java
+++ src/main/java/megabytesme/minelights/config/MineLightsConfig.java
@@ -4,15 +4,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MineLightsConfig {
+    public transient boolean refreshDevices = false;
     public transient boolean restartProxy = false;
     public transient boolean restartProxyAsAdmin = false;
     public transient boolean clearDisabledDevices = false;
 
     public boolean enableMod = true;
+
+    public boolean enableCorsair = true;
+    public boolean enableAsus = true;
+    public boolean enableLogitech = true;
+    public boolean enableRazer = true;
+    public boolean enableWooting = true;
+    public boolean enableSteelSeries = true;
+    public boolean enableMsi = true;
+    public boolean enableNovation = true;
+    public boolean enablePicoPi = true;
     public boolean enableOpenRgb = true;
-    public boolean enableIcueProxy = true;
-    public boolean enableMysticLightProxy = true;
-    public boolean enableAuraSdk = true;
     public List<String> disabledDevices = new ArrayList<>();
 
     public boolean enableHealthBar = true;
