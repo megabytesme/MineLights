76bc494500f72e83311c10ff61159c49e4f74354
MegaBytesMe <57240557+megabytesme@users.noreply.github.com>
2025-09-30 17:42:47 +0100

    Added target for 1.17 (1.17.x to 1.18.x)

--- Client/stonecutter.gradle.kts
+++ Client/stonecutter.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     // id("me.modmuss50.mod-publish-plugin") version "0.8.+" apply false // Publishes builds to hosting websites
 }
 
-stonecutter active "1.16.2"
+stonecutter active "1.17"
 
 /*
 // Make newer versions be published last
